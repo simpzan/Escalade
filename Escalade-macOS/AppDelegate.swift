9001776c63ff9c5bfb31bd6fc6c8888e03157edb
Samuel Zhang <simpzan@gmail.com>
2017-02-07 22:03:00 +0800

    add macOS app target generated from template.

---
+++ Escalade-macOS/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Escalade-macOS
+//
+//  Created by Samuel Zhang on 2/7/17.
+//
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
