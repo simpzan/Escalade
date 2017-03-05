1ffeac7b1dc5f4e4d7000b36e9490c06da294a4f
Samuel Zhang <simpzan@gmail.com>
2017-03-05 17:05:16 +0800

    add skeleton code to make PacketTunnel working.

--- Escalade-iOS/ViewController.swift
+++ Escalade-iOS/ViewController.swift
@@ -7,8 +7,60 @@
 //
 
 import UIKit
+import NetworkExtension
 
 class ViewController: UIViewController {
+    func createManager(callback: @escaping (NETunnelProviderManager?) -> Void) {
+        let config = NETunnelProviderProtocol()
+        config.providerBundleIdentifier = "com.simpzan.Escalade-iOS.PacketTunnel-iOS"
+        config.serverAddress = "10.0.0.2"
+
+        let manager = NETunnelProviderManager()
+        manager.protocolConfiguration = config
+        manager.isEnabled = true
+        manager.saveToPreferences { (error) in
+            if error == nil {
+                callback(manager)
+            } else {
+                NSLog("create manager error \(error)")
+                callback(nil)
+            }
+        }
+    }
+
+    func loadManager(callback: @escaping (NETunnelProviderManager?) -> Void) {
+        NETunnelProviderManager.loadAllFromPreferences { managers, error in
+            if error == nil {
+                callback(managers?.first)
+            } else {
+                NSLog("load managers failed \(error)")
+                callback(nil)
+            }
+        }
+    }
+    func getManager(callback: @escaping (NETunnelProviderManager?) -> Void) {
+        loadManager { manager in
+            if manager != nil { return callback(manager) }
+
+            self.createManager { manager in
+                self.loadManager(callback: callback)
+            }
+        }
+    }
+
+    @IBAction func tunnel(_ sender: Any) {
+        getManager { manager in
+            if manager == nil { return NSLog("get manager failed") }
+
+            do {
+                try manager!.connection.startVPNTunnel(options: nil)
+                NSLog("started")
+            } catch {
+                NSLog("start error \(error)")
+            }
+        }
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- PacketTunnel-iOS/PacketTunnelProvider.swift
+++ PacketTunnel-iOS/PacketTunnelProvider.swift
@@ -9,7 +9,40 @@
 import NetworkExtension
 
 class PacketTunnelProvider: NEPacketTunnelProvider {
+    public let httpProxyAddress = "127.0.0.1"
+    public let httpProxyPort: UInt16 = 9090
+    public let socksProxyPort: UInt16 = 9091
+
+    func getTunnelSettings() -> NEPacketTunnelNetworkSettings {
+        let settings = NEPacketTunnelNetworkSettings(tunnelRemoteAddress: "192.0.2.2")
+        settings.mtu = 1600
+        settings.dnsSettings = NEDNSSettings(servers: ["114.114.114.114"])
+        let v4Settings = NEIPv4Settings(addresses:["192.0.2.1"], subnetMasks:["255.255.255.0"])
+        v4Settings.includedRoutes = [NEIPv4Route.default()]
+        v4Settings.excludedRoutes = [NEIPv4Route(destinationAddress:"114.114.114.114", subnetMask:"255.255.255.255")]
+        settings.iPv4Settings = v4Settings
+        settings.proxySettings = getProxySettings()
+        return settings
+    }
+    func getProxySettings() -> NEProxySettings {
+        let proxy = NEProxySettings()
+        proxy.httpEnabled = true
+        proxy.httpServer = NEProxyServer(address: httpProxyAddress, port: Int(httpProxyPort))
+        proxy.httpsEnabled = true
+        proxy.httpsServer = proxy.httpServer
+        return proxy
+    }
+
     override func startTunnel(options: [String : NSObject]? = nil, completionHandler: @escaping (Error?) -> Void) {
+        NSLog("startTunnel \(options)")
+        setTunnelNetworkSettings(getTunnelSettings()) { (error) in
+            if error != nil {
+                NSLog("\(#function) error:\(error)")
+                return
+            }
+            NSLog("connected")
+            completionHandler(nil)
+        }
     }
 
     override func stopTunnel(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
