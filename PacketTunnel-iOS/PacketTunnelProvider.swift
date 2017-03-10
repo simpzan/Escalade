a9935438bcf885995e069043d72af169fc738989
Samuel Zhang <simpzan@gmail.com>
2017-03-10 16:41:25 +0800

    move vpn service logic to VPNManager class.

--- PacketTunnel-iOS/PacketTunnelProvider.swift
+++ PacketTunnel-iOS/PacketTunnelProvider.swift
@@ -11,43 +11,15 @@ import NEKit
 import CocoaLumberjackSwift
 
 class PacketTunnelProvider: NEPacketTunnelProvider {
-    func getFactory(host: String, port: Int, encryption: String, password: String) -> AdapterFactory {
-        let protocolObfuscaterFactory = ShadowsocksAdapter.ProtocolObfuscater.OriginProtocolObfuscater.Factory()
-        let streamObfuscaterFactory = ShadowsocksAdapter.StreamObfuscater.OriginStreamObfuscater.Factory()
-        let algorithm = CryptoAlgorithm(rawValue: encryption.uppercased())
-        let cryptoFactory = ShadowsocksAdapter.CryptoStreamProcessor.Factory(password: password, algorithm: algorithm!)
-        return ShadowsocksAdapterFactory(serverHost: host,
-                                        serverPort: port,
-                                        protocolObfuscaterFactory: protocolObfuscaterFactory,
-                                        cryptorFactory: cryptoFactory,
-                                        streamObfuscaterFactory: streamObfuscaterFactory)
+
+    override class func initialize() {
+        DDLog.add(DDTTYLogger.sharedInstance()) // TTY = Xcode console
+        DDLog.add(DDASLLogger.sharedInstance()) // ASL = Apple System Logs
     }
-    func setupRuleManager() {
-        let ssAdapterFactory = getFactory(host: "cn2t-52.hxg.cc",
-                                          port: 59671,
-                                          encryption: "rc4-md5",
-                                          password: "")
-        let chinaRule = CountryRule(countryCode: "CN", match: true, adapterFactory: DirectAdapterFactory())
-        let allRule = AllRule(adapterFactory: ssAdapterFactory)
-        let manager = RuleManager(fromRules: [chinaRule, allRule], appendDirect: true)
-        RuleManager.currentManager = manager
-    }
-    func startProxyServer() {
-        NSLog("startProxyServer")
 
-        setupRuleManager()
-
-        httpProxyServer = GCDHTTPProxyServer(address: IPAddress(fromString: httpProxyAddress), port: Port(port: httpProxyPort))
-        try! httpProxyServer!.start()
-
-        NSLog("proxy started!")
-    }
-    var httpProxyServer: GCDProxyServer?
-
-
-    public let httpProxyAddress = "127.0.0.1"
-    public let httpProxyPort: UInt16 = 9090
-    public let socksProxyPort: UInt16 = 9091
+    lazy var manager: VPNManager = {
+        VPNManager(provider: self)
+    }()
 
     func getTunnelSettings() -> NEPacketTunnelNetworkSettings {
         let settings = NEPacketTunnelNetworkSettings(tunnelRemoteAddress: "192.0.2.2")
@@ -55,7 +27,7 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
         settings.dnsSettings = NEDNSSettings(servers: ["114.114.114.114"])
         let v4Settings = NEIPv4Settings(addresses:["192.0.2.1"], subnetMasks:["255.255.255.0"])
         v4Settings.includedRoutes = [NEIPv4Route.default()]
-//        v4Settings.excludedRoutes = [NEIPv4Route(destinationAddress:"114.114.114.114", subnetMask:"255.255.255.255")]
+        v4Settings.excludedRoutes = [NEIPv4Route(destinationAddress:"114.114.114.114", subnetMask:"255.255.255.255")]
         settings.iPv4Settings = v4Settings
         settings.proxySettings = getProxySettings()
         return settings
@@ -63,7 +35,7 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
     func getProxySettings() -> NEProxySettings {
         let proxy = NEProxySettings()
         proxy.httpEnabled = true
-        proxy.httpServer = NEProxyServer(address: httpProxyAddress, port: Int(httpProxyPort))
+        proxy.httpServer = NEProxyServer(address: manager.httpProxyAddress, port: Int(manager.httpProxyPort))
         proxy.httpsEnabled = true
         proxy.httpsServer = proxy.httpServer
 //        proxy.autoProxyConfigurationEnabled = true
@@ -76,69 +48,20 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
     override func startTunnel(options: [String : NSObject]? = nil, completionHandler: @escaping (Error?) -> Void) {
         NSLog("startTunnel \(options)")
 
-        DDLog.add(DDTTYLogger.sharedInstance()) // TTY = Xcode console
-        DDLog.add(DDASLLogger.sharedInstance()) // ASL = Apple System Logs
-        ObserverFactory.currentFactory = DebugObserverFactory()
+        manager.start()
 
-        self.startProxyServer()
         setTunnelNetworkSettings(getTunnelSettings()) { (error) in
             if error != nil {
                 NSLog("\(#function) error:\(error)")
                 return
             }
-            self.startPacketProcessor()
-            self.listenReachabilityChange()
             NSLog("connected")
             completionHandler(nil)
         }
     }
 
-    func startPacketProcessor() {
-        Opt.MAXNWTCPSocketReadDataSize = 60 * 1024
-        RawSocketFactory.TunnelProvider = self
-        self.interface = TUNInterface(packetFlow: self.packetFlow)
-
-        let ipRange = try! IPRange(startIP: IPAddress(fromString: "198.18.1.1")!, endIP: IPAddress(fromString: "198.18.255.255")!)
-        let fakeIPPool = IPPool(range: ipRange)
-        let dnsServer = DNSServer(address: IPAddress(fromString: "114.114.114.114")!, port: Port(port: 53), fakeIPPool: fakeIPPool)
-        let resolver = UDPDNSResolver(address: IPAddress(fromString: "114.114.114.114")!, port: Port(port: 53))
-        dnsServer.registerResolver(resolver)
-        self.interface.register(stack: dnsServer)
-        DNSServer.currentServer = dnsServer
-
-        let udpStack = UDPDirectStack()
-        self.interface.register(stack: udpStack)
-
-        let tcpStack = TCPStack.stack
-        tcpStack.proxyServer = self.httpProxyServer
-        self.interface.register(stack: tcpStack)
-        self.interface.start()
-    }
-    var interface: TUNInterface!
-
-    func restartProxy() {
-        NSLog("restarting proxy")
-        interface.stop()
-        httpProxyServer?.stop()
-        try! httpProxyServer?.start()
-        interface.start()
-    }
-
-    func listenReachabilityChange() {
-        func onReachabilityChange(_: Any) {
-            DispatchQueue.main.async {
-                self.restartProxy()
-            }
-        }
-        reachability.whenReachable = onReachabilityChange
-        reachability.whenUnreachable = onReachabilityChange
-        try? reachability.startNotifier()
-    }
-    let reachability = Reachability()!
-
     override func stopTunnel(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
-        interface.stop()
-        httpProxyServer?.stop()
+        manager.stop()
         completionHandler()
     }
 
@@ -154,4 +77,5 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
 
 	override func wake() {
 	}
+
 }
---
+++ PacketTunnel-iOS/VPNManager.swift
@@ -0,0 +1,115 @@
+//
+//  VPNManager.swift
+//  Escalade
+//
+//  Created by Samuel Zhang on 3/10/17.
+//
+//
+
+import Foundation
+import CocoaLumberjackSwift
+import NetworkExtension
+import NEKit
+
+class VPNManager {
+    private func getFactory(host: String, port: Int, encryption: String, password: String) -> AdapterFactory {
+        let protocolObfuscaterFactory = ShadowsocksAdapter.ProtocolObfuscater.OriginProtocolObfuscater.Factory()
+        let streamObfuscaterFactory = ShadowsocksAdapter.StreamObfuscater.OriginStreamObfuscater.Factory()
+        let algorithm = CryptoAlgorithm(rawValue: encryption.uppercased())
+        let cryptoFactory = ShadowsocksAdapter.CryptoStreamProcessor.Factory(password: password, algorithm: algorithm!)
+        return ShadowsocksAdapterFactory(serverHost: host,
+                                         serverPort: port,
+                                         protocolObfuscaterFactory: protocolObfuscaterFactory,
+                                         cryptorFactory: cryptoFactory,
+                                         streamObfuscaterFactory: streamObfuscaterFactory)
+    }
+    private func setupRuleManager() {
+        let ssAdapterFactory = getFactory(host: "cn2t-52.hxg.cc",
+                                          port: 59671,
+                                          encryption: "rc4-md5",
+                                          password: "")
+        let chinaRule = CountryRule(countryCode: "CN", match: true, adapterFactory: DirectAdapterFactory())
+        let allRule = AllRule(adapterFactory: ssAdapterFactory)
+        let manager = RuleManager(fromRules: [chinaRule, allRule], appendDirect: true)
+        RuleManager.currentManager = manager
+    }
+    func startProxyServer() {
+        NSLog("startProxyServer")
+
+        setupRuleManager()
+
+        httpProxyServer = GCDHTTPProxyServer(address: IPAddress(fromString: httpProxyAddress), port: Port(port: httpProxyPort))
+        try! httpProxyServer!.start()
+
+        NSLog("proxy started!")
+    }
+    var httpProxyServer: GCDProxyServer?
+
+
+    public let httpProxyAddress = "127.0.0.1"
+    public let httpProxyPort: UInt16 = 9090
+    public let socksProxyPort: UInt16 = 9091
+
+
+    func startPacketProcessor(packetFlow: NEPacketTunnelFlow) {
+        Opt.MAXNWTCPSocketReadDataSize = 60 * 1024
+        interface = TUNInterface(packetFlow: packetFlow)
+
+        let ipRange = try! IPRange(startIP: IPAddress(fromString: "198.18.1.1")!, endIP: IPAddress(fromString: "198.18.255.255")!)
+        let fakeIPPool = IPPool(range: ipRange)
+        let dnsServer = DNSServer(address: IPAddress(fromString: "114.114.114.114")!, port: Port(port: 53), fakeIPPool: fakeIPPool)
+        let resolver = UDPDNSResolver(address: IPAddress(fromString: "114.114.114.114")!, port: Port(port: 53))
+        dnsServer.registerResolver(resolver)
+        interface.register(stack: dnsServer)
+        DNSServer.currentServer = dnsServer
+
+        let udpStack = UDPDirectStack()
+        interface.register(stack: udpStack)
+
+        let tcpStack = TCPStack.stack
+        tcpStack.proxyServer = httpProxyServer
+        interface.register(stack: tcpStack)
+
+        interface.start()
+    }
+    private var interface: TUNInterface!
+
+
+    let packetFlow: NEPacketTunnelFlow
+    init(provider: NEPacketTunnelProvider) {
+        RawSocketFactory.TunnelProvider = provider
+        packetFlow = provider.packetFlow
+        ObserverFactory.currentFactory = DebugObserverFactory()
+    }
+
+    public func start() {
+        setupRuleManager()
+        startProxyServer()
+        startPacketProcessor(packetFlow: packetFlow)
+        DDLogInfo("vpn started")
+    }
+
+    public func stop() {
+        interface?.stop()
+        httpProxyServer?.stop()
+        DDLogInfo("vpn stopped")
+    }
+
+    public func restart() {
+//        httpProxyServer?.stop()
+//        try! httpProxyServer?.start()
+    }
+
+    func listenReachabilityChange() {
+        func onReachabilityChange(_: Any) {
+            DispatchQueue.main.async {
+                self.restart()
+            }
+        }
+        reachability.whenReachable = onReachabilityChange
+        reachability.whenUnreachable = onReachabilityChange
+        try? reachability.startNotifier()
+    }
+    let reachability = Reachability()!
+
+}
