12a34e110d94cc3e364c43f4b82d249659fc16e0
Samuel Zhang <simpzan@gmail.com>
2017-03-12 17:04:16 +0800

    refactoring and simplify TCPStack.

--- src/IPStack/TCPStack.swift
+++ src/IPStack/TCPStack.swift
@@ -5,42 +5,20 @@ import CocoaLumberjackSwift
 /// This class wraps around tun2socks to build a TCP only IP stack.
 open class TCPStack: TSIPStackDelegate, IPStackProtocol {
     /// The `TCPStack` singleton instance.
-    open static var stack: TCPStack {
-        TSIPStack.stack.delegate = _stack
-        TSIPStack.stack.processQueue = QueueFactory.getQueue()
-        return _stack
-    }
-    fileprivate static let _stack: TCPStack = TCPStack()
-    
-    /// The proxy server that handles connections accepted from this stack.
-    ///
-    /// - warning: This must be set before `TCPStack` is registered to `TUNInterface`.
-    open weak var proxyServer: ProxyServer?
-    
-    /// This is set automatically when the stack is registered to some interface.
-    open var outputFunc: (([Data], [NSNumber]) -> Void)! {
-        get {
-            return TSIPStack.stack.outputBlock
-        }
-        set {
-            TSIPStack.stack.outputBlock = newValue
-        }
-    }
-    
-    /**
-     Inistailize a new TCP stack.
-     */
-    fileprivate init() {
-    }
-    
+    open static var stack = TCPStack()
+
+    private let ipStack = TSIPStack.stack
+
+    // MARK: - input/output
+
     /**
      Input a packet into the stack.
-     
+
      - note: Only process IPv4 TCP packet as of now, since stable lwip does not support ipv6 yet.
-     
+
      - parameter packet:  The IP packet.
      - parameter version: The version of the IP packet, i.e., AF_INET, AF_INET6.
-     
+
      - returns: If the stack takes in this packet. If the packet is taken in, then it won't be processed by other IP stacks.
      */
     open func input(packet: Data, version: NSNumber?) -> Bool {
@@ -51,32 +29,47 @@ open class TCPStack: TSIPStackDelegate, IPStackProtocol {
             }
         }
         if IPPacket.peekProtocol(packet) == .tcp {
-            TSIPStack.stack.received(packet: packet)
+            ipStack.received(packet: packet)
             return true
         }
         return false
     }
-    
-    public func start() {
-        TSIPStack.stack.resumeTimer()
+
+    /// This is set automatically when the stack is registered to some interface.
+    open var outputFunc: (([Data], [NSNumber]) -> Void)! {
+        get { return ipStack.outputBlock }
+        set { ipStack.outputBlock = newValue }
     }
-    
+
+    // MARK: - start/stop
+
+    public func start() {
+        ipStack.delegate = self
+        ipStack.processQueue = QueueFactory.getQueue()
+        ipStack.resumeTimer()
+    }
+
     /**
      Stop the TCP stack.
-     
+
      After calling this, this stack should never be referenced. Use `TCPStack.stack` to get a new reference of the singleton.
      */
     open func stop() {
-        TSIPStack.stack.delegate = nil
-        TSIPStack.stack.suspendTimer()
+        ipStack.delegate = nil
+        ipStack.suspendTimer()
         proxyServer = nil
     }
-    
-    // MARK: TSIPStackDelegate Implementation
+
+    // MARK: - TSIPStackDelegate Implementation
     open func didAcceptTCPSocket(_ sock: TSTCPSocket) {
         DDLogDebug("Accepted a new socket from IP stack.")
         let tunSocket = TUNTCPSocket(socket: sock)
         let proxySocket = DirectProxySocket(socket: tunSocket)
-        self.proxyServer!.didAcceptNewSocket(proxySocket)
+        proxyServer?.didAcceptNewSocket(proxySocket)
     }
+
+    /// The proxy server that handles connections accepted from this stack.
+    ///
+    /// - warning: This must be set before `TCPStack` is registered to `TUNInterface`.
+    open weak var proxyServer: ProxyServer?
 }
