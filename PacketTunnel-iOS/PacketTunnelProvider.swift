251be0872eadca351a91e28ab70372540a075a25
Samuel Zhang <simpzan@gmail.com>
2017-03-14 17:31:21 +0800

    custom log formatter for file logger to include log level.

--- PacketTunnel-iOS/PacketTunnelProvider.swift
+++ PacketTunnel-iOS/PacketTunnelProvider.swift
@@ -10,15 +10,42 @@ import NetworkExtension
 import NEKit
 import CocoaLumberjackSwift
 
+extension DDLogFlag: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case DDLogFlag.error:   return "E"
+        case DDLogFlag.warning: return "W"
+        case DDLogFlag.info:    return "I"
+        case DDLogFlag.debug:   return "D"
+        case DDLogFlag.verbose: return "V"
+        default:                return " "
+        }
+    }
+}
+class LogFormatter: NSObject, DDLogFormatter {
+    let formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(identifier: "Asia/Shanghai")
+        formatter.dateFormat = "MM-dd HH:mm:ss:SSS"
+        return formatter
+    }()
+    public func format(message logMessage: DDLogMessage!) -> String! {
+        let time = formatter.string(from: logMessage.timestamp)
+        let file = "\(logMessage.fileName!):\(logMessage.line)"
+        return "\(time) \(logMessage.flag) \(file) \t\(logMessage.message ?? "")"
+    }
+}
+
 class PacketTunnelProvider: NEPacketTunnelProvider {
 
-    private var logFile: String? = {
+    private let logFile: String? = {
         DDLog.add(DDTTYLogger.sharedInstance()) // TTY = Xcode console
         DDLog.add(DDASLLogger.sharedInstance()) // ASL = Apple System Logs
 
         let logger = DDFileLogger()!
         logger.rollingFrequency = TimeInterval(60*60*12)
         logger.logFileManager.maximumNumberOfLogFiles = 3
+        logger.logFormatter = LogFormatter()
         DDLog.add(logger, with: .debug)
 
         return logger.logFileManager?.sortedLogFilePaths.first
