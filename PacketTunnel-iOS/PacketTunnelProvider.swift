ebc5f3dca909d80e6edc6ffea7c4ccf2fc6007ed
Samuel Zhang <simpzan@gmail.com>
2017-03-18 21:30:26 +0800

    PacketTunnel switch to ProxyService.

--- PacketTunnel-iOS/PacketTunnelProvider.swift
+++ PacketTunnel-iOS/PacketTunnelProvider.swift
@@ -23,33 +23,13 @@ private let fileLogger: DDFileLogger? = {
 }()
 
 class PacketTunnelProvider: NEPacketTunnelProvider {
-
-    lazy var manager: VPNManager = {
-        VPNManager(provider: self)
+    private lazy var proxyService: ProxyService? = {
+        let configManager = ConfigurationManager()
+        if !configManager.reloadConfigurations() { return nil }
+        return ProxyService(config: configManager.current!, provider: self)
     }()
-
-    func getTunnelSettings() -> NEPacketTunnelNetworkSettings {
-        let settings = NEPacketTunnelNetworkSettings(tunnelRemoteAddress: "192.0.2.2")
-        settings.mtu = 1500
-        settings.dnsSettings = NEDNSSettings(servers: ["114.114.114.114"])
-        let v4Settings = NEIPv4Settings(addresses:["192.0.2.1"], subnetMasks:["255.255.255.0"])
-        v4Settings.includedRoutes = [NEIPv4Route.default()]
-//        v4Settings.excludedRoutes = [NEIPv4Route(destinationAddress:"114.114.114.114", subnetMask:"255.255.255.255")]
-        settings.iPv4Settings = v4Settings
-//        settings.proxySettings = getProxySettings()
-        return settings
-    }
-    func getProxySettings() -> NEProxySettings {
-        let proxy = NEProxySettings()
-        proxy.httpEnabled = true
-        proxy.httpServer = NEProxyServer(address: manager.httpProxyAddress, port: Int(manager.httpProxyPort))
-        proxy.httpsEnabled = true
-        proxy.httpsServer = proxy.httpServer
-//        proxy.autoProxyConfigurationEnabled = true
-//        proxy.proxyAutoConfigurationURL
-        proxy.excludeSimpleHostnames = true
-        proxy.matchDomains = []
-        return proxy
+    var tunController: TUNController {
+        return (proxyService?.tunController)!
     }
 
     private var logFile: String? {
@@ -61,9 +41,9 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
         NSLog("log file \(logFile)")
 
         self.addObserver(self, forKeyPath: "defaultPath", options: [.new], context: nil)
-        manager.start()
+        proxyService?.start()
 
-        setTunnelNetworkSettings(getTunnelSettings()) { (error) in
+        setTunnelNetworkSettings(tunController.getTunnelSettings()) { (error) in
             if error != nil {
                 DDLogError("setTunnelNetworkSettings error:\(error)")
                 return
@@ -76,7 +56,7 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
         DDLogInfo("stopTunnel \(self) \(reason)")
         NSLog("log file \(logFile)")
         self.removeObserver(self, forKeyPath: "defaultPath")
-        manager.stop()
+        proxyService?.stop()
         completionHandler()
     }
 
@@ -88,18 +68,18 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
 
     override public func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         DDLogDebug("defaultPath changed")
-        manager.reset()
+        proxyService?.restart()
     }
 
     override func sleep(completionHandler: @escaping () -> Void) {
         DDLogInfo("about to sleep...")
-        manager.stop()
+        proxyService?.stop()
         completionHandler()
     }
 
     override func wake() {
         DDLogInfo("about to wake...")
-        manager.start()
+        proxyService?.start()
     }
 
     deinit {
