9f4f98dd912becf99ad6e2bd4f5ad176d715956f
simpzan <simpzan@gmail.com>
2018-06-27 16:51:04 +0800

    retain fake ip pools when DNSServer.stop() called.

--- src/IPStack/DNS/DNSServer.swift
+++ src/IPStack/DNS/DNSServer.swift
@@ -151,7 +151,7 @@ open class DNSServer: DNSResolverDelegate, IPStackProtocol {
         resolvers = []
 
         pendingSessions = [:]
-        fakeSessions = [:]
+//        fakeSessions = [:]
         // The blocks scheduled with `dispatch_after` are ignored since they are hard to cancel. But there should be no consequence, everything will be released except for a few `IPAddress`es and the `queue` which will be released later.
     }
 
