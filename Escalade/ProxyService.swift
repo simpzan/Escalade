9d5ec083a6f2380e9f58bb9f2781c0efee100343
simpzan <simpzan@gmail.com>
2018-07-15 17:36:59 +0800

    add domain rules for the built-in rules.

--- Escalade/ProxyService.swift
+++ Escalade/ProxyService.swift
@@ -29,8 +29,31 @@ class ProxyService {
         RuleManager.currentManager = createDefaultRules(adapterFactoryManager: adapterFactoryManager)
     }
     
+    private func createDomainRule(ruleFile: String, adapter: AdapterFactory) -> Rule? {
+        do {
+            let filepath = Bundle.main.path(forResource: ruleFile, ofType: "rule")!
+            let content = try String(contentsOfFile: filepath)
+            let regexs = content.components(separatedBy: CharacterSet.newlines)
+            let criteria = try! regexs.filter { (regex: String) -> Bool in
+                return !regex.isEmpty
+            }.map { (regex: String) -> DomainListRule.MatchCriterion in
+                let re = try NSRegularExpression(pattern: regex, options: .caseInsensitive)
+                return .regex(re)
+            }
+            return DomainListRule(adapterFactory: adapter, criteria: criteria)
+        } catch let error {
+            DDLogError("createDomainRule \(ruleFile) failed, \(error)")
+            return nil
+        }
+    }
     private func createDefaultRules(adapterFactoryManager: AdapterFactoryManager) -> RuleManager {
         var rules: [Rule] = []
+        if let directRule = createDomainRule(ruleFile: "DirectDomain", adapter: adapterFactoryManager["direct"]!) {
+            rules.append(directRule)
+        }
+        if let proxyRule = createDomainRule(ruleFile: "ProxyDomain", adapter: adapterFactoryManager["proxy"]!) {
+            rules.append(proxyRule)
+        }
         let chinaRule = CountryRule(countryCode: "CN", match: true, adapterFactory: adapterFactoryManager["direct"]!)
         rules.append(chinaRule)
         let intranetRule = CountryRule(countryCode: "--", match: true, adapterFactory: adapterFactoryManager["direct"]!)
