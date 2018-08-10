167a139dce208b3f28a1eee3fe4fdbef9846cad5
simpzan <simpzan@gmail.com>
2018-08-10 21:57:38 +0800

    add test case: when vpn enabled, ping twitter and qq ok.

---
+++ EscaladeTests-macOS/VPNSpec.swift
@@ -0,0 +1,63 @@
+//
+//  VPNSpec.swift
+//  EscaladeTests-macOS
+//
+//  Created by simpzan on 2018/8/10.
+//
+
+import Quick
+import Nimble
+import NetworkExtension
+@testable import Escalade_macOS
+
+class VPNSpec: QuickSpec {
+    override func spec() {
+        let vpn = VPNManager.shared
+        beforeEach {
+            waitUntil(timeout: 5) { (done) in
+                NSLog("ensure vpn enabled")
+                if vpn.connected { return done() }
+
+                vpn.monitorStatus(callback: { (status) in
+                    if status == .connected { done() }
+                })
+                vpn.startVPN()
+            }
+        }
+        afterEach {
+            waitUntil(timeout: 3, action: { (done) in
+                NSLog("disabling vpn")
+                vpn.monitorStatus(callback: { (status) in
+                    if status == .disconnected { done() }
+                })
+                vpn.stopVPN()
+            })
+        }
+        describe("when vpn enabled") {
+            it("ping twitter ok.") {
+                self.urlSessionHttpPing(url: "http://twitter.com")
+            }
+            it("ping qq ok, too.") {
+                self.urlSessionHttpPing(url: "http://qq.com")
+            }
+        }
+    }
+    
+    func urlSessionHttpPing(url: String) {
+        waitUntil(timeout: 3, action: { (done) in
+            self.httpPing(url: url, done: { (result) in
+                expect(result) == true
+                done()
+            })
+        })
+    }
+    func httpPing(url: String, done: @escaping (Bool) -> Void) {
+        let task = URLSession.shared.dataTask(with: URL(string: url)!) { (data, response, err) in
+            NSLog("\(data) \(response) \(err)")
+            let res = response as! HTTPURLResponse
+            done(res.statusCode == 200)
+        }
+        task.resume()
+    }
+}
+
