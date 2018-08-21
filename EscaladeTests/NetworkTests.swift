aac9fad4bcc4d44a5a575b9ed61e1a9d1d3da378
Samuel Zhang <simpzan@gmail.com>
2018-08-21 22:47:25 +0800

    add data and hex string conversion functions.

--- EscaladeTests/NetworkTests.swift
+++ EscaladeTests/NetworkTests.swift
@@ -59,6 +59,44 @@ class DispatchQueueTests: QuickSpec {
     }
 }
 
+extension String {
+    /// Create `Data` from hexadecimal string representation
+    /// This takes a hexadecimal representation and creates a `Data` object. Note, if the string
+    /// has any spaces or non-hex characters (e.g. starts with '<' and with a '>'), those are
+    /// ignored and only hex characters are processed.
+    /// - returns: Data represented by this hexadecimal string.
+    func hexadecimal() -> Data? {
+        var data = Data(capacity: count / 2)
+        let regex = try! NSRegularExpression(pattern: "[0-9a-f]{1,2}", options: .caseInsensitive)
+        regex.enumerateMatches(in: self, range: NSMakeRange(0, utf16.count)) { match, flags, stop in
+            let byteString = (self as NSString).substring(with: match!.range)
+            var num = UInt8(byteString, radix: 16)!
+            data.append(&num, count: 1)
+        }
+        return data.count > 0 ? data : nil
+    }
+}
+extension Data {
+    /// Create hexadecimal string representation of `Data` object.
+    /// - returns: `String` representation of this `Data` object.
+    func hexadecimal() -> String {
+        return map { String(format: "%02x", $0) }
+            .joined(separator: "")
+    }
+}
+class DataHexStringTests: QuickSpec {
+    override func spec() {
+        it("data hexadecimal string test") {
+            let hexString = "030305ac 00000000 4500007e 00000000 4011d389 72727272 c0000201 0035f6b1 006a0000"
+            let hex = hexString.replacingOccurrences(of: " ", with: "")
+            let data = hex.hexadecimal()!
+            expect(data.count) == 36
+            let hex2 = data.hexadecimal()
+            expect(hex2) == hex
+        }
+    }
+}
+
 class NetworkTests: QuickSpec {
     override func spec() {
         it("dns ok") {
